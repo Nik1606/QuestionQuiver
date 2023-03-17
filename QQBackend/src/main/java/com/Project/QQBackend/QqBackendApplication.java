8541b2537b56ccf4ae9ec4eeba17bb2d99913525
Nikhil <nikhilthorat1996@gmail.com>
2023-03-17 14:17:42 +0530

    Spring starter project created. application.properties file updated to do MySQL configuration

---
+++ QQBackend/src/main/java/com/Project/QQBackend/QqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Project.QQBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QqBackendApplication.class, args);
+	}
+
+}
